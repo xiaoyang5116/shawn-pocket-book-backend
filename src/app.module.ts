a4fd2d3afebe106db685e85c750a63a921044d27
shawn <1443584075@qq.com>
2023-07-09 19:48:21 +0800

    feat: 连接 mysql

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,32 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        logging: true,
+        poolSize: 10,
+        connectorPackage: 'mysql2',
+        extra: {
+          authPlugin: 'sha256_password',
+        },
+        autoLoadEntities: true, // 有助于自动加载模块, 而不是手动输入实体数组 "entities": [],
+        synchronize: true, // 确保 Typeorm 实体在每次运行程序时都会与数据库同步, 生产环境要关闭为 false
+      }),
+    }),
+    ConfigModule.forRoot(),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,9 +1,41 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+
+@Entity()
 export class User {
+  @PrimaryGeneratedColumn()
   id: number;
-  name: string;
-  email: string;
+
+  @Column({
+    length: 20,
+    comment: '用户名',
+  })
+  username: string;
+
+  @Column({
+    length: 20,
+    comment: '密码',
+  })
   password: string;
+
+  @Column({
+    comment: '头像',
+  })
   avatar: string;
-  createTime: Date;
+
+  @Column({
+    length: 20,
+    nullable: true,
+    comment: '个性签名',
+  })
   signature?: string;
+
+  @CreateDateColumn({
+    comment: '创建时间',
+  })
+  createTime: Date;
 }
