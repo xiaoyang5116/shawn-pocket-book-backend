25fe3b98743aff6f8db089e14cd0b100993d59a0
shawn <1443584075@qq.com>
2023-07-22 10:08:17 +0800

    fix: 修改用户注册默认头像

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -14,8 +14,7 @@ function md5(str: string): string {
   return hash.digest('hex');
 }
 
-const defaultAvatar =
-  'http://s.yezgea02.com/1615973940679/WeChat77d6d2ac093e247c361f0b8a7aeb6c2a.png';
+const defaultAvatar = 'static/uploads/avatar/default_avatar.png';
 
 @Injectable()
 export class UserService {
